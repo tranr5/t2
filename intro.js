f1d79e63fa7ee182f38debf84b7e5135402624bd
tranr5 <tranr4@gmail.com>
2025-06-02 12:37:18 -0400

    yes

--- intro.js
+++ intro.js
@@ -2,7 +2,52 @@
 
 let x = 25
  x = 100
-console.log(x)
+// console.log(x)
 
 let name = "Randy"
-console.log(name)
+// console.log(name)
+
+function checkSquare(x) {
+    const sqrt = Math.sqrt(x);
+    if (sqrt % 1 === 0) {
+        // console.log("good")
+    } else {
+        // console.log("not good")
+    }
+}
+
+function checkSquares(x) {
+    return Math.sqrt(x) % 1 === 0
+}
+
+
+// console.log(checkSquares(9))
+
+function checkLimit(x) { //can put checkSqure in
+    let perfectSquares = [];
+    for (let i = 0; i <= x; i++) {
+        if (Math.sqrt(i) % 1 === 0) {
+            perfectSquares.push(i)
+        }
+    }
+    return perfectSquares
+}
+
+function checkLimits(x) { //can put checkSqure in
+    let perfectSquares = [];
+    for (let i = 0; i <= x; i++) {
+        if (checkSquares(i)) {
+            perfectSquares.push(i)
+        }
+    }
+    return perfectSquares
+}
+
+
+// console.log(checkLimit(100))
+
+function reverseMessage(message) {
+    return message.split("").reverse()
+}
+
+console.log(reverseMessage("hi dog"))
