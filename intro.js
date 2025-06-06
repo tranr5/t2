bb8d6ad6899b275ae84a0de74a259e7195d4c39e
tranr5 <tranr4@gmail.com>
2025-06-06 12:13:41 -0400

    yes

--- intro.js
+++ intro.js
@@ -46,8 +46,35 @@ function checkLimits(x) { //can put checkSqure in
 
 // console.log(checkLimit(100))
 
-function reverseMessage(message) {
-    return message.split("").reverse()
+// function reverseMessage(message) {
+//     return message.split("").reverse()
+// }
+
+// console.log(reverseMessage("hi dog"))
+
+
+// function multiplyTogether(a,b=2) {
+//     return a*b
+// }
+
+// console.log(multiplyTogether(2))
+
+// function getArray(array) {
+//     let sum = 0
+//     for(let i=0; i<array.length; i++) {
+//         sum = array[i] + sum
+//     }
+//     return sum
+// }
+
+// console.log(getArray([2,4,3]))
+
+function arrayifyString(string) {
+  let x = string.replace(/[^A-Za-z0-9]+/g, " ");
+    let newArr = x.trim().split(" ")
+    return newArr;
+  //  your code here!
+  //  Want an additional challenge? - don't use the built in JS string method Array.split(). 
 }
 
-console.log(reverseMessage("hi dog"))
+console.log(arrayifyString("they is it"))
